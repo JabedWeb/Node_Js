e6157e790f7fbb6b8386d05aba5ca5f34517fc2b
jabed366 <jabedhasan231@gmail.com>
2022-05-30 20:58:24 +0600

    First Commit

---
+++ ServerMake/server.js
@@ -0,0 +1,20 @@
+import http from 'http';
+import dotenv from 'dotenv';
+import fs from 'fs'
+
+//environmental init
+dotenv.config();
+const PORT=process.env.SERVER_PORT
+
+//Data Management
+const students_json=fs.readFileSync('./data/students.json')
+const students_obj=JSON.parse(students_json);
+console.log(students_obj);
+
+//Create Server
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type' : 'application/json'});
+    res.end(students_obj);
+}).listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+})
